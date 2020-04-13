ccf4c9035dd79ff5a3c6ddb8458804e23ce90a99
Gabriel Onike <onikegabriel@gmail.com>
2020-04-13 08:03:33 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,6 +3,16 @@
 end
 
 def current_player(board)
-  turn = turn_count(board)
-  turn.odd? ? "O" : "X"
+  turn_count(board).odd? ? "O" : "X"
 end
+
+#AN ALTERNATIVE turn count below
+#def turn_count(board)
+#    count = 0
+#    board.each do |move|
+#      if move == "X" || move == "O"
+#        count += 1
+#      end
+#    end
+#count
+#end
